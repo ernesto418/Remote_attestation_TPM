c2df66a55c21eab071c22085ae4ee72ac5996715
ernesto418 <60478028+ernesto418@users.noreply.github.com>
2021-04-29 17:40:07 +0100

    Update 1_cleanup.sh

--- Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/1_cleanup.sh
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/1_cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
-rm -f credential.blob ek.crt ek.pub ak.pub ak.name pcr quote sig qualification config.cfg binary_runtime_measurements
+rm -f credential.blob ek.crt ek.pub ak.pub ak.name pcr quote sig qualification \
+config.cfg binary_runtime_measurements authorization_signature PuK.pem \
+server_pubkey.ctx end_user.ctx server_pubkey.name
 cp ../config.cfg ./config.cfg
-
+tpm2_createprimary -C o -g sha256 -G ecc256 -c end_user.ctx
