518c639ce3c96ad4583ee137eebb917e83d21258
Ernesto Gomez Marin <60478028+ernesto418@users.noreply.github.com>
2021-06-16 14:54:22 +0200

    Update tools_installation.sh
    The key providing goes in key_providing.sh

--- Provisioning_Raspberry_Pi_4/tools_installation.sh
+++ Provisioning_Raspberry_Pi_4/tools_installation.sh
@@ -55,18 +55,3 @@ sudo ldconfig
 cd ..
 
 reboot
-
-##TPM providing
-echo "Creating keys"
-cd remote_attestation_tools
-sudo chmod a+rw /dev/tpm0
-sudo chmod a+rw /dev/tpmrm0
-#Warning: clear the platform hierarchy authorization values!
-tpm2_clear -c p
-tpm2_createek -G rsa -u ek.pub -c ek.ctx
-tpm2_evictcontrol -C o -c ek.ctx 0x81010001
-tpm2_createak -C 0x81010001 -c ak.ctx -G rsa -g sha256 -s rsassa -u ak.pub -n ak.name
-tpm2_evictcontrol -C o -c ak.ctx 0x81000002
-
-echo "Keys created"
-
