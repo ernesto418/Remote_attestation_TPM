4be2af930ea753b7f9a4011a5d65ef7a3e6d5192
Ernesto Gomez Marin <60478028+ernesto418@users.noreply.github.com>
2021-09-09 17:39:35 +0200

    Update 3_attune.sh

--- Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/3_attune.sh
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/3_attune.sh
@@ -6,6 +6,15 @@ tpm2_nvread 0x1c00002 -o ek.crt
 # Send AIK public key and expected PCRs value to server...
 ./attune
 
+#Decrytp PuB key showing that Ak is inside the TPM
+echo Decrypting PuB key
+tpm2_startauthsession --policy-session -S session.ctx
+tpm2_policysecret -S session.ctx -c 0x4000000B
+tpm2_activatecredential -c 0x81000002 -C 0x81010001 -i credential.blob -o qualification -P"session:session.ctx"
+tpm2_flushcontext session.ctx
+rm session.ctx
+
+
 #Load public key 
 echo Load public key 
 tpm2_loadexternal -G rsa -C o -u PuK.pem -c server_pubkey.ctx -n server_pubkey.name
