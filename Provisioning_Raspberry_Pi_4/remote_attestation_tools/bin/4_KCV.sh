f5ac0fa57a52755de150438f5fd8ea2ecf8c388b
Ernesto Gomez Marin <60478028+ernesto418@users.noreply.github.com>
2021-09-24 13:25:07 +0200

    Update 4_KCV.sh

--- Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/4_KCV.sh
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/4_KCV.sh
@@ -1,21 +1,34 @@
 #!/bin/sh -x
 
+
+#Decrypting Auth public key showing that Ak.pub was valid
+echo Decrypting Auth public key
+tpm2_startauthsession --policy-session -S session.ctx
+tpm2_policysecret -S session.ctx -c 0x4000000B
+tpm2_activatecredential -c 0x81000002 -C 0x81010001 -i auth.secret -o auth.pem -P"session:session.ctx"
+tpm2_flushcontext session.ctx
+rm session.ctx
+
+
 #Load public key 
-echo Load public key 
-tpm2_loadexternal -G rsa -C o -u PuK.pem -c server_pubkey.ctx -n server_pubkey.name
+echo Loading Auth public key into the TPM
+tpm2_loadexternal -G rsa -C o -u auth.pem -c auth.ctx -n auth.name
 
 #Create authorization session with the public key
-echo Create authorization session with the public key
+echo Createing authorization policy with the public key
 tpm2_startauthsession -S session.ctx
 
-tpm2_policyauthorize -S session.ctx -L authorized.policy -n server_pubkey.name
+tpm2_policyauthorize -S session.ctx -L authorized.policy -n auth.name
 
 tpm2_flushcontext session.ctx
 
-#We create the RSA key pair under the authorization policy 
-echo We create the RSA key pair under the authorization policy (Sealed Key)
-tpm2_create -G rsa -u SeK.pub -r Sek.priv -L authorized.policy -c Sek.ctx -C end_user.ctx
+#We create the ECC key pair under the authorization policy in the owner hierarchy
+tpm2_createprimary -c end_user.ctx -C o 
+
+
+echo Creating and loading ECC-256 key pair under the authorization policy (Sealed Key)
+tpm2_create -C end_user.ctx -G ecc256  -u SeK.pub -r Sek.priv -L authorized.policy -c Sek.ctx 
 
 
 # Send SeK public key
-./KCV_1
+./KCV
