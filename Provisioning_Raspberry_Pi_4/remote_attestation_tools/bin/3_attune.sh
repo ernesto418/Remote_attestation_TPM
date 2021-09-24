ba3fecfa9c40d54577490030f5ef5c52accd543c
Ernesto Gomez Marin <60478028+ernesto418@users.noreply.github.com>
2021-09-24 15:44:00 +0200

    Update 3_attune.sh

--- Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/3_attune.sh
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/3_attune.sh
@@ -15,22 +15,3 @@ tpm2_flushcontext session.ctx
 rm session.ctx
 
 
-#Load public key 
-echo Load public key 
-tpm2_loadexternal -G rsa -C o -u PuK.pem -c server_pubkey.ctx -n server_pubkey.name
-
-#Create authorization session with the public key
-echo Create authorization session with the public key
-tpm2_startauthsession -S session.ctx
-
-tpm2_policyauthorize -S session.ctx -L authorized.policy -n server_pubkey.name
-
-tpm2_flushcontext session.ctx
-
-#We create the RSA key pair under the authorization policy 
-echo We create the RSA key pair under the authorization policy 
-tpm2_create -G rsa -u pubkey.pub -r prikey.pub -L authorized.policy -c rsa.ctx -C end_user.ctx
-
-#This process is only valid if a CA create a certificate for this key pair created
-
-
