c6a118979aefccc6e106fcd933951e329e3ecddf
Ernesto Gomez Marin <60478028+ernesto418@users.noreply.github.com>
2021-09-24 16:52:12 +0200

    Update 4_KCV.sh

--- Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/4_KCV.sh
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/4_KCV.sh
@@ -25,10 +25,12 @@ tpm2_flushcontext session.ctx
 #We create the ECC key pair under the authorization policy in the owner hierarchy
 tpm2_createprimary -c end_user.ctx -C o 
 
-
 echo Creating and loading ECC-256 key pair under the authorization policy (Sealed Key)
 tpm2_create -C end_user.ctx -G ecc256  -u SeK.pub -r Sek.priv -L authorized.policy -c Sek.ctx 
 
+#Certify the correct characteritics of Sealed key throught the atetstation key
+
+tpm2_certify -C ak.ctx -c Sek.ctx  -g sha256 -o attest.out -s sig.out
 
 # Send SeK public key
 ./KCV
