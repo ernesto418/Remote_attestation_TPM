1f6c74110d6081d73604712bfbbbce299d3ae2a9
Ernesto Gomez Marin <60478028+ernesto418@users.noreply.github.com>
2021-09-27 15:17:57 +0200

    Update 3_attune.sh

--- Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/3_attune.sh
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/3_attune.sh
@@ -10,7 +10,7 @@ tpm2_nvread 0x1c00002 -o ek.crt
 echo Decrypting PuB key
 tpm2_startauthsession --policy-session -S session.ctx
 tpm2_policysecret -S session.ctx -c 0x4000000B
-tpm2_activatecredential -c 0x81000002 -C 0x81010001 -i credential.blob -o qualification -P"session:session.ctx"
+tpm2_activatecredential -c 0x81000002 -C 0x81010001 -i PuK.credential -o PuK.pem -P"session:session.ctx"
 tpm2_flushcontext session.ctx
 rm session.ctx
 
