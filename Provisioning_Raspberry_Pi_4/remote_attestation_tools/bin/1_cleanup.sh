eedb3bcccda992ac35104b42bcb6b4182e103f4c
ernesto418 <ernestogm96@gmail.com>
2021-10-21 10:46:13 +0100

    PCR and Reset policies finished V1.0

--- Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/1_cleanup.sh
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/1_cleanup.sh
@@ -4,7 +4,8 @@ rm -f credential.blob ek.crt ek.pub ak.pub ak.name pcr quote sig qualification \
 config.cfg binary_runtime_measurements authorization_signature PuK.credential AuthPuK.pem \
 server_pubkey.ctx end_user.ctx server_pubkey.name AESkey.credential AESkey.bin \
 AuthPuK_encrypted.b64 auth.ctx auth.name auth.pub authorized.policy session.ctx certSig.out \
-SeK.* SeKcert.out ../certificates/ucsr.bin auth_sign_bin sig.SeK
+SeK.* SeKcert.out ../certificates/ucsr.bin auth_sign_bin sig.SeK \
+pcrreset_policy.digest verification.tkt message.dat sig.SeK
 
 cp ../config.cfg ./config.cfg
 #Creating primary here? is it no also in 4_KCV? to be checked
---
+++ Provisioning_Raspberry_Pi_4/remote_attestation_tools/bin/test_SeK.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -x
+
+echo "my message" > message.dat
+reset=$(tpm2_readclock | grep reset_count: | grep -o '[0-9]*')
+
+# generating tpm2_Policypcr and tpm2_policycountertimer digest
+
+tpm2_startauthsession -S session.ctx --hash-algorithm="sha256"
+
+tpm2_policypcr -S session.ctx -l "sha256:10"
+tpm2_policycountertimer -S session.ctx resets=$reset -L pcrreset_policy.digest
+
+tpm2_flushcontext session.ctx
+
+# Verifying authorization signature
+echo "verifying signature"
+tpm2_verifysignature -c auth.ctx -g sha256 -m pcrreset_policy.digest -s auth_sign_bin -t verification.tkt -f rsassa
+
+# Start session
+
+tpm2_startauthsession --policy-session -S session.ctx --hash-algorithm="sha256"
+
+# Generate current policypcr and policyreset (Real!!)
+echo "Satisfying policies"
+tpm2_policypcr -S session.ctx -l "sha256:10"
+tpm2_policycountertimer -S session.ctx resets=$reset
+
+echo "policies satisfied"
+# Demostrating that we have authorization to use our key in the current policypcr
+
+tpm2_policyauthorize -S session.ctx -L authorized.policy -i pcrreset_policy.digest -n auth.name -t verification.tkt
+
+# We use the Sealed key
+
+tpm2_sign -p"session:session.ctx" -c SeK.ctx -g sha256 -o sig.SeK message.dat
+
+tpm2_flushcontext session.ctx
+
+# Verify signature
+
+tpm2_verifysignature -c SeK.ctx -g sha256 -s sig.SeK -m message.dat
