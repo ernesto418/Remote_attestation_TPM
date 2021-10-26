cb238a308141be9c297a3e4e841396693dae9fd5
Ernesto Gomez Marin <60478028+ernesto418@users.noreply.github.com>
2021-10-26 16:42:57 +0200

    Update some bugs, and changing download source to download just specific commit

--- Kernel_building/kernel_64bit_installation.sh
+++ Kernel_building/kernel_64bit_installation.sh
@@ -8,11 +8,16 @@ sudo umount /dev/$RNAME
 set -e
 #Check the correct state of the sd card
 echo "Testing SD card state"
+set +e
+sudo rm -r mnt_test
+set -e
 mkdir mnt_test
 mkdir mnt_test/fat32
 mkdir mnt_test/ext4
 sudo mount /dev/$BNAME mnt_test/fat32
+sleep 1
 sudo mount /dev/$RNAME mnt_test/ext4
+sleep 2
 sudo umount /dev/$BNAME
 sudo umount /dev/$RNAME
 sudo rm -r mnt_test
@@ -27,13 +32,21 @@ export PATH=$PATH:~/tools/arm-bcm2708/arm-linux-gnueabihf/bin
 KERNEL=kernel8
 
 #Download kernel
-git clone -b rpi-4.19.y https://github.com/raspberrypi/linux
-cd linux
-git checkout 06606627043f72d22881563d485268fec2acd56d
+set +e
+sudo rm -r linux
+set -e
+sudo curl -L https://github.com/raspberrypi/linux/archive/06606627043f72d22881563d485268fec2acd56d.zip --output linux.zip
+unzip linux.zip && mv linux-06606627043f72d22881563d485268fec2acd56d linux
 
+cd linux
 #First installation
+echo "----------------------------------- First installation -----------------------------------------------"
+
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+set +e
+sudo rm -r mnt
+set -e
 mkdir mnt
 mkdir mnt/fat32
 mkdir mnt/ext4
@@ -51,12 +64,14 @@ sudo umount mnt/fat32
 sudo umount mnt/ext4
 
 #Adding parchs
+echo "----------------------------------- Parching -----------------------------------------------"
 sudo cp ../parch_64/clk-bcm2835.c drivers/clk/bcm/clk-bcm2835.c
 sudo cp ../parch_64/.config .config
 sudo cp ../parch_64/ima_policy.c security/integrity/ima/ima_policy.c
 
 #Second installation
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+echo "----------------------------------- Second installation -----------------------------------------------"
+sudo make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
 
 sudo mount /dev/$BNAME mnt/fat32
 sudo mount /dev/$RNAME mnt/ext4
